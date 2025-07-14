84cf5b986e13e0fb11987ed5769164fe7842f6b6
Fortrend96 <serbin-96@mail.ru>
2025-07-14 12:09:21 +0700

    First triangle.

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,47 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// массив вершин
+GLfloat points[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
+
+// массив цветов вершин
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+// шейдеры
+
+// вертексный шейдер
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main() {"
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_position, 1.0);"
+"}";
+
+// фрагментный шейдер
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main() {"
+"   frag_color = vec4(color, 1.0);"
+"}";
 
 // размеры окна
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
 
-void glfwWindowZieCallback(GLFWwindow* pWindow, int width, int height)
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
 {
     g_windowSizeX = width;
     g_windowSizeY = height;
@@ -18,6 +53,7 @@ void glfwWindowZieCallback(GLFWwindow* pWindow, int width, int height)
 
 void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
 {
+    // если нажата клавиша esc, то выставляем флаг закрытия окна в true
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     {
         glfwSetWindowShouldClose(pWindow, GL_TRUE);
@@ -49,7 +85,7 @@ int main(void)
     }
 
     // регистрация коллбэков
-    glfwSetWindowSizeCallback(pWindow, glfwWindowZieCallback);
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
     glfwSetKeyCallback(pWindow, glfwKeyCallback);
 
     // Делаем контекст окна OpenGL текущим
@@ -66,11 +102,58 @@ int main(void)
 
 	glClearColor(1,1,0,1);
 
-   // цикл отрисовки на экране
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER); // id вертексного шейдера
+    glShaderSource(vs, 1, &vertex_shader, nullptr); // передаем исходный код вертексного шейдера
+    glCompileShader(vs); // компиляция вертексного шейдера
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER); // id фрагментного шейдера
+    glShaderSource(fs, 1, &fragment_shader, nullptr); // передаем исходный код фрагментного шейдера
+    glCompileShader(fs); // компиляция фрагментного шейдера
+
+
+    GLuint shader_program = glCreateProgram(); // Создаёт новую пустую шейдерную программу и возвращает её идентификатор
+    glAttachShader(shader_program, vs); // Прикрепляет вертексный шейдер к программе
+    glAttachShader(shader_program, fs); // Прикрепляет фрагментный шейдер к программе
+    glLinkProgram(shader_program); // линковка шейдерной программы
+
+    // удаление шейдеров
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    GLuint points_vbo = 0; // ID буфера
+    glGenBuffers(1, &points_vbo);  // создаёт 1 буферный объект, и его ID записывается в points_vbo
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo); // Привязывает созданный буфер как текущий буфер для вершинных данных
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW); // Загружает данные из массива points в GPU.
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);  
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao); //  Создаёт VAO и сохраняет его ID в переменную vao
+    glBindVertexArray(vao); // Активирует VAO — теперь вся последующая настройка будет привязана к нему
+
+    // Вершины
+    glEnableVertexAttribArray(0); //  включает определённый входной атрибут (layout(location = 0) в шейдере).
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo); // Привязывает буфер вершин (VBO)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr); // Описывает, как из буфера points_vbo читать данные для атрибута 0 // 3 компонента (x, y, z), тип float, без смещений
+
+    // Цвета
+    glEnableVertexAttribArray(1); // Включает использование атрибута с индексом 1 (обычно — цвет)
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo); // Привязывает буфер цветов
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    // цикл отрисовки на экране
     while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES,0,3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
